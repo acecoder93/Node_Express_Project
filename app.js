4d08747b6d9ae31fefa88857b26f0b3acfca4ea4
Anuj <anujsaheba@gmail.com>
2019-01-09 14:08:11 -0600

    adding form

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 
-
-
 // Setting up Routers
 app.use(express.static('public'));
 app.use(require('./routes/index.js'))
 app.use(require('./routes/cryptocurrency.js'))
+app.use(require('./routes/feedback.js'))
 
 app.listen(5000, ()=>{
     console.log('Listening on port 5000')
---
+++ routes/feedback.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const body_parser = require('body_parser');
+
+
+router.get('/feedback', (req, res)=>{
+    res.render('feedback');
+})
+
+
+router.use(body_parser.urlencoded({extended: false}));
+
+router.post('/feedback', (req, res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phoneNumber = req.body.number;
+    var comments = req.body.comments;
+
+    res.send(`${name} ${email} ${phoneNumber} ${comments}`);
+
+})
+
+module.exports = router;
