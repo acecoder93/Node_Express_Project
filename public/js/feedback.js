0932fdc43efb87b83c7aaac2d1089de6235b7853
Anuj <anujsaheba@gmail.com>
2019-01-17 17:48:42 -0600

    need to update jquery for submit and deletion

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -49,17 +49,6 @@ $.getJSON('/api', updateFeedback);
             output += '       </div>';
             output += '     </div>';
 
-            // output += '     <div class="feedback-item item-list media-list">';
-            // output += '       <div class="feedback-item media">';
-            // output += '       <div class="media-left"><button><span id="' + key + '" class="glyphicon glyphicon-remove"></span></button></div>';
-            // output += '         <div class="feedback-info media-body">';
-            // output += '           <div class="feedback-head">';
-            // output += '             <div class="feedback-title">' + item.name + ' <small class="feedback-name label label-info">' + item.name + '</small></div>';
-            // output += '           </div>';
-            // output += '           <div class="feedback-message">' + item.email + item.phoneNumber + item.comments + '</div>';
-            // output += '         </div>';
-            // output += '       </div>';
-            // output += '     </div>';
           });
 
             // output += '<button><span id="delete-button' + key + '></span></button>';
